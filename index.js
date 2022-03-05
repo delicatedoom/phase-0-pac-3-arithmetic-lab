1aae70faf5bf44ff14af4c3a72c0e13454f40e11
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-03-05 15:33:41 -0700

    commit tree 6272afecee14a7a676645f83f24bcef095145af3

---
+++ index.js
@@ -0,0 +1,30 @@
+function add() {
+    // we'll fill this in shortly
+  }
+function subtract() {
+    // we'll fill this in shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function divide() {
+    //we'll fill this in shortly
+  }
+  function add(1, 4) {
+    return a + b;
+  }
+  function subtract(a, b) {
+      return a - b;
+  }
+  function multiply(a, b) {
+      return a * b;
+  }
+  function divide( a, b) {
+      return a / b;
+  }
+  function add(n) {
+      return n + a;
+  }
+  function subtract(n) {
+      return n - a;
+  }
